2d777a7bfbac6081ac23c5ec9f5b0c3aa1335037
Rui Ferreira <ruijmaferreira@gmail.com>
2022-08-02 16:05:54 -0300

    useState

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import NotificationButton from '../NotificationButton'
+import NotificationButton from '../NotificationButton';
 import './styles.css'
 
 function SalesCard() {
+
+    const max = new Date();
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
+
     return (
         <div className="dsmeta-container">
             <div className="dsmeta-card">
@@ -11,18 +19,20 @@ function SalesCard() {
                 <div>
                     <div className="dsmeta-form-control-container">
                         <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => { }}
+                            selected={minDate}
+                            onChange={(date: Date) => setMinDate(date)}
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
-                        />                    </div>
+                        />
+                    </div>
                     <div className="dsmeta-form-control-container">
                         <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => { }}
+                            selected={maxDate}
+                            onChange={(date: Date) => setMaxDate(date)}
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
-                        />                    </div>
+                        />                   
+                    </div>
                 </div>
 
                 <div>
@@ -101,4 +111,4 @@ function SalesCard() {
     )
 }
 
-export default SalesCard
+export default SalesCard;
