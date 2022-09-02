e945be97ccf4dbbdf8e4ef1a6e8c5fa1d6223fc5
Rui Ferreira <ruijmaferreira@gmail.com>
2022-09-02 15:30:26 -0300

    Security config

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton';
-import './styles.css'
+import './styles.css';
 
 function SalesCard() {
 
